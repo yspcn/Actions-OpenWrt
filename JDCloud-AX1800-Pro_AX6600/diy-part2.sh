521e028fc903602dd755bf4aeb39ea2bba6ca5bd
yspcn <55094985+yspcn@users.noreply.github.com>
2024-08-15 12:52:36 +0800

    Update diy-part2.sh

--- JDCloud-AX1800-Pro_AX6600/diy-part2.sh
+++ JDCloud-AX1800-Pro_AX6600/diy-part2.sh
@@ -11,7 +11,28 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i 's/ImmortalWrt/JDC-BaiLi/g' package/base-files/files/bin/config_generate
+
+# Modify default golang
+rm -rf feeds/packages/lang/golang
+git clone --depth=1 -b 23.05 https://github.com/Lienol/openwrt-packages #https://github.com/immortalwrt/packages
+cp -rf openwrt-packages/lang/golang feeds/packages/lang/  && rm -rf openwrt-packages
+sed -i 's/ +libopenssl-legacy//g' package/small/shadowsocksr-libev/Makefile
+
+# tailscale install
+mkdir -p package/utils/ucode
+wget -P package/utils/ucode/ https://github.com/openwrt/openwrt/raw/openwrt-22.03/package/utils/ucode/Makefile
+#git clone --depth=1 -b openwrt-23.05 https://github.com/openwrt/packages packages-temp
+#rm -rf feeds/packages/net/tailscale && cp -rf packages-temp/net/tailscale feeds/packages/net/
+#rm -rf packages-temp
+rm -f feeds/packages/net/tailscale/Makefile
+wget -P feeds/packages/net/tailscale/ https://github.com/openwrt/packages/raw/openwrt-23.05/net/tailscale/Makefile
+sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+rm -rf package/feeds/luci/luci-app-ipsec-vpnd package/feeds/luci/luci-app-ipsec-vpnserver-manyusers package/openwrt-packages/luci-app-homeproxy
+
 ##-----------------Add dev core for kenzo OpenClash------------------
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
