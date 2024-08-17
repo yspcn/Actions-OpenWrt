26bf20e9fa6ecfd5058c9da31da86b2460186e30
yspcn <55094985+yspcn@users.noreply.github.com>
2024-08-17 09:49:21 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili/diy-part2.sh
+++ JDCloud-AX6000-Baili/diy-part2.sh
@@ -26,7 +26,7 @@ sed -i 's/ImmortalWrt/JDC-BaiLi/g' package/base-files/files/bin/config_generate
 mkdir -p package/utils/ucode
 wget -P package/utils/ucode/ https://github.com/openwrt/openwrt/raw/openwrt-22.03/package/utils/ucode/Makefile
 #git clone --depth=1 -b openwrt-23.05 https://github.com/openwrt/packages packages-temp
-git clone --depth=1 -b openwrt-23.05 https://github.com/openwrt/packages packages-temp
+git clone --depth=1 https://github.com/coolsnowwolf/packages packages-temp
 rm -rf feeds/packages/net/tailscale && cp -rf packages-temp/net/tailscale feeds/packages/net/
 rm -rf packages-temp
 rm -f feeds/packages/net/tailscale/Makefile
