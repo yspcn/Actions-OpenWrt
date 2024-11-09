1e3dd34d0ae7176cf629d831272dc8f87e624772
yspcn <55094985+yspcn@users.noreply.github.com>
2024-11-09 15:53:02 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili-immortalwrt/diy-part1.sh
+++ JDCloud-AX6000-Baili-immortalwrt/diy-part1.sh
@@ -19,3 +19,7 @@
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 git clone https://github.com/gdy666/luci-app-lucky package/lucky
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone --depth=1 https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone --depth=1 https://github.com/kenzok8/small package/small
