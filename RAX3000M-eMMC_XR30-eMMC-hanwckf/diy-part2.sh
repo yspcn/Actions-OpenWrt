24f5e44a79f1e9284bf4d5d8b5983174ce680a50
yspcn <55094985+yspcn@users.noreply.github.com>
2025-09-25 12:44:09 +0800

    Update diy-part2.sh

--- RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part2.sh
+++ RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part2.sh
@@ -27,7 +27,10 @@ sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
 sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="1.3GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-rm -f feeds/packages/net/tailscale/Makefile
+rm -f feeds/packages/net/tailscale/Makefile && rm -f feeds/packages/net/tailscale/files/tailscale.init
 wget -P feeds/packages/net/tailscale https://github.com/asvow/neo-addon/raw/refs/heads/main/tailscale/Makefile
-sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+wget -P feeds/packages/net/tailscale/files https://github.com/asvow/neo-addon/raw/refs/heads/main/tailscale/files/tailscale.helper
+wget -P feeds/packages/net/tailscale/files https://github.com/asvow/neo-addon/raw/refs/heads/main/tailscale/files/tailscale.init
+wget -P feeds/packages/net/tailscale/files https://github.com/asvow/neo-addon/raw/refs/heads/main/tailscale/files/tailscale.iface
+# sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
 git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
