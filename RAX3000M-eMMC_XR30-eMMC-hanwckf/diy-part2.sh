7b525323e359bb147f8bfd4940916d991cebbe36
yspcn <55094985+yspcn@users.noreply.github.com>
2025-11-19 10:33:20 +0800

    Replace Tailscale repository with community version

--- RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part2.sh
+++ RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part2.sh
@@ -28,5 +28,4 @@ sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="1.3GHz" ;;/}' package/
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
-# sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
-git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+git clone https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community package/luci-app-tailscale-community
